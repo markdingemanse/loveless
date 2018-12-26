9f5fbdab98da18d874e098693da64ab560cf04e1
Mark Dingemanse <dingemansemark@gmail.com>
2018-12-26 13:29:34 +0100

    cleaned up the base class

--- main.go
+++ main.go
@@ -8,22 +8,34 @@ import (
 );
 
 func main() {
-        r := gin.Default();
-
-        // Ping handler
-        r.GET("/", func(c *gin.Context) {
-                c.String(200, "pong");
-        })
-
-        var mode string;
-        mode = os.Getenv("LOVELESS_MODE");
+        router := routes(router());
+        mode   := getAppMode();
 
         fmt.Println("[MAIN] Running Gin in mode: " + mode);
 
         if (mode == "dev") {
-            r.Run();
+            router.Run();
         } else {
             // log.Fatal(autotls.Run(r, "lovelesswired.com", "www.lovelesswired.com"));
-            autotls.Run(r, "lovelesswired.com", "www.lovelesswired.com");
+            autotls.Run(router, "lovelesswired.com", "www.lovelesswired.com");
         }
+}
+
+// Bootstraps the basic gin router.
+func router() *gin.Engine {
+    return gin.Default();
+}
+
+// Provisions the provided router with the needed routes.
+func routes(router *gin.Engine) *gin.Engine {
+    router.GET("/", func(c *gin.Context) {
+            c.String(200, "pong");
+    });
+
+    return router;
+}
+
+// Get the current app mode we are running under.
+func getAppMode() string {
+    return os.Getenv("LOVELESS_MODE");
 }
